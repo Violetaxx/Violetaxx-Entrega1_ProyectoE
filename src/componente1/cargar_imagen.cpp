7b612c74c4fd6cf3e263be58e9d063a201b15f4e
Violeta <violeta.fajardo@javeriana.edu.co>
2025-03-03 15:32:06 -0500

    Update cargar_imagen.cpp

--- src/componente1/cargar_imagen.cpp
+++ src/componente1/cargar_imagen.cpp
@@ -1,1 +1,41 @@
+#include "Imagen.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//constructor
+Imagen::Imagen() : ancho(0), alto(0), maxVal(0){}
+
+//operacion para cargar una imagen desde un archivo PGM
+bool Imagen::cargar(const string& nombreArchivo){
+    ifstream archivo(nombreArchivo);
+    if(!archivo) {
+        cout<<"Error: No se pudo abrir el archivo"<<nombreArchivo <<endl;
+        return false;
+    }
+
+    string linea;
+    archivo>>linea;
+    if(linea != "P2"){
+        cout<<"Error: Formato de archivo no soportado"<<endl;
+        return false;
+    }
+
+    archivo>>ancho>>alto;
+    archivo>> maxVal;
+
+    //redimensionar la matriz de píxeles
+    pixeles.resize(alto, vector<int>(ancho));
+
+    //leer los valores de los píxeles
+    for(int i = 0; i < alto; ++i){
+        for(int j = 0; j < ancho; ++j){
+            archivo>>pixeles[i][j];
+        }
+    }
+
+    nombre= nombreArchivo;
+    cout<<"La imagen "<<nombreArchivo<<" ha sido cargada."<<endl;
+    return true;
+}
 
