fe55a43427efd40c98623e8562102bd12439f25e
Violeta <violeta.fajardo@javeriana.edu.co>
2025-03-03 21:48:33 -0500

    Update Imagen.h

--- src/componente1/Imagen.h
+++ src/componente1/Imagen.h
@@ -1,24 +1,34 @@
 #ifndef IMAGEN_H
 #define IMAGEN_H
-using namesapce std;
 
 #include <string>
 #include <vector>
 
-class Imagen {
+class Imagen{
 private:
-    string nombre;
-    int ancho;
-    int alto;
-    int maxVal;
-    vector<vector<int>> pixeles;
+    std::string nombreImagen;
+    int altoImagen;
+    int anchoImagen;
+    std::vector<std::vector<int>> pixeles;
 
 public:
-    Imagen(); //constructor
+    Imagen();//constructor
+
+    //getters
+    std::string getNombreImagen() const;
+    int getAltoImagen() const;
+    int getAnchoImagen() const;
+    std::vector<std::vector<int>> getPixeles() const;
+
+    //setters
+    void setNombreImagen(const std::string& nombre);
+    void setAltoImagen(int alto);
+    void setAnchoImagen(int ancho);
+    void setPixeles(const std::vector<std::vector<int>>& pixeles);
 
     //operaciones
-    bool cargar(string& nomArchi);
-    void mostrarInfo();
+    bool cargar(const std::string& nombreArchivo);
+    void mostrarInfo() const;
 };
 
-#endif
+#endif // IMAGEN_H
