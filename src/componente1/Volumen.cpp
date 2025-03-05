56963404e9b59d44e5b5c8d5d00c9d78ea55aa1f
Violeta <violeta.fajardo@javeriana.edu.co>
2025-03-04 22:17:50 -0500

    Update Volumen.cpp

--- src/componente1/Volumen.cpp
+++ src/componente1/Volumen.cpp
@@ -1,15 +1,20 @@
 #include "Volumen.h"
+#include <iostream>
 
-//operación para mostrar información de el volumen
-void Volumen::info_volumen(const vector<Imagen>& volumen, const string& nombreBase) {
-    if (volumen.empty()) {
-        cout << "No hay un volumen cargado." << endl;
+//constructor
+Volumen::Volumen(const std::string& nombreBase) : nombreBase(nombreBase) {}
+
+
+//operacion para mostrar información del volumen
+void Volumen::info_volumen() const {
+    if (imagenes.empty()) {
+        cout << "No hay un volumen cargado." << std::endl;
         return;
     }
 
     cout << "Información del volumen:" << endl;
     cout << "Nombre base: " << nombreBase << endl;
-    cout << "Cantidad de imágenes: " << volumen.size() << endl;
-    cout << "Ancho de las imágenes: " << volumen[0].getAnchoImagen() << " píxeles" << endl;
-    cout << "Alto de las imágenes: " << volumen[0].getAltoImagen() << " píxeles" << endl;
+    cout << "Cantidad de imágenes: " << imagenes.size() << endl;
+    cout << "Ancho de las imágenes: " << imagenes[0].getAnchoImagen() << " píxeles" << endl;
+    cout << "Alto de las imágenes: " << imagenes[0].getAltoImagen() << " píxeles" << endl;
 }
