15e3bf306e0a9a9b1bdd91e35b6d1d751d385777
Violeta <violeta.fajardo@javeriana.edu.co>
2025-03-04 22:02:35 -0500

    Update Volumen.cpp

--- src/componente1/Volumen.cpp
+++ src/componente1/Volumen.cpp
@@ -1,1 +1,15 @@
+#include "Volumen.h"
 
+//operación para mostrar información de el volumen
+void Volumen::info_volumen(const vector<Imagen>& volumen, const string& nombreBase) {
+    if (volumen.empty()) {
+        cout << "No hay un volumen cargado." << endl;
+        return;
+    }
+
+    cout << "Información del volumen:" << endl;
+    cout << "Nombre base: " << nombreBase << endl;
+    cout << "Cantidad de imágenes: " << volumen.size() << endl;
+    cout << "Ancho de las imágenes: " << volumen[0].getAnchoImagen() << " píxeles" << endl;
+    cout << "Alto de las imágenes: " << volumen[0].getAltoImagen() << " píxeles" << endl;
+}
