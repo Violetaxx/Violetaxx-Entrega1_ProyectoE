b91ef9f31f0b7cce1e4c844e635ed7a9462340f6
Violeta <violeta.fajardo@javeriana.edu.co>
2025-03-04 22:27:12 -0500

    Update comandos.h

--- src/comandos.h
+++ src/comandos.h
@@ -1,1 +1,18 @@
+#ifndef COMANDOS_H_INCLUDED
+#define COMANDOS_H_INCLUDED
 
+#include <string>
+#include <vector>
+#include "Imagen.h"
+#include "Volumen.h"
+
+Imagen cargar_imagen(const std::string& nombreArchivo);
+void info_imagen(const Imagen& imagen);
+Volumen cargar_volumen(const std::string& nombreBase, int cantidadImagenes);
+void info_volumen(const Volumen& volumen);
+
+void f_ayuda();
+void f_ayudaComando(int opcion);
+void f_procesarComando(int opcion, const std::string& parametro);
+
+#endif // COMANDOS_H_INCLUDED
